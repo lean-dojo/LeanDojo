5b989ba9f0faa6de4fa81ebb5d0ed32c6a73f4e5
Kaiyu Yang <kaiyuy@caltech.edu>
2023-08-15 07:02:04 -0700

    Update build_docker_image.sh

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/sh
+# This is for developers only. Users do not have to build Docker containers.
+# To use LeanDojo with Docker, users just have to make sure Docker is installed and running.
 docker buildx build --platform linux/amd64,linux/arm64 -t yangky11/lean-dojo:latest --push - < docker/Dockerfile
