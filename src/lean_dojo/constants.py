d4adeeed22e722daae60fff4ab33293e7bf3ebc6
josojo <josojo@hotmail.de>
2023-09-14 00:39:26 +0200

    Faster recurisve loading of indexed content

--- src/lean_dojo/constants.py
+++ src/lean_dojo/constants.py
@@ -83,6 +83,9 @@ LEAN4_NIGHTLY_REPO = GITHUB.get_repo("leanprover/lean4-nightly")
 LEAN4_DEPS_DIR = Path("lake-packages")
 """The directory where Lean 4 dependencies are stored."""
 
+LOAD_TRACED_DEPENDENCIES_RECURSIVELY = bool(os.getenv("LOAD_TRACED_DEPENDENCIES_RECURSIVELY", False))
+"""Whether to load traced dependencies recursively."""
+
 TACTIC_TIMEOUT = int(os.getenv("TACTIC_TIMEOUT", 5000))
 """Maximum time (in milliseconds) before interrupting a tactic when interacting with Lean (only for Lean 3).
 """
