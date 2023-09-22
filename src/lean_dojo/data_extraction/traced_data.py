11a618742ce46fac9a3316af5fe46f8a79eebfb8
Kaiyu Yang <kaiyuy@caltech.edu>
2023-09-22 13:44:55 -0700

    increase i

--- src/lean_dojo/data_extraction/traced_data.py
+++ src/lean_dojo/data_extraction/traced_data.py
@@ -1257,6 +1257,8 @@ def _build_dependency_graph(
                 traced_files.append(tf_dep)
 
             G.add_edge(tf_path_str, dep_path_str)
+   
+        i += 1
 
     assert nx.is_directed_acyclic_graph(G)
     return G
