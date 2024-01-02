2feb385619ac9efd3dc0faf686af9d10060c3542
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-01-02 22:32:56 +0800

    fix #118

--- src/lean_dojo/data_extraction/build_lean4_repo.py
+++ src/lean_dojo/data_extraction/build_lean4_repo.py
@@ -134,7 +134,9 @@ def is_new_version(v) -> bool:
     assert major == 4 and minor == 3 and patch == 0
     if "4.3.0-rc" in v:
         rc = int(v.split("-")[1][2:])
-    return rc >= 2
+        return rc >= 2
+    else:
+        return True
 
 
 def main() -> None:
