9ebdf31358eb61e02725bec04d8950344e36ed76
Kaiyu Yang <kaiyuy@caltech.edu>
2024-02-27 08:54:17 -0800

    minor fix

--- tests/interaction/test_commands.py
+++ tests/interaction/test_commands.py
@@ -4,7 +4,7 @@ from lean_dojo import *
 
 
 def test_example_1(mathlib4_repo: LeanGitRepo) -> None:
-    entry = (mathlib4_repo, "Mathlib/LinearAlgebra/Basic.lean", 83)
+    entry = (mathlib4_repo, "Mathlib/LinearAlgebra/Basic.lean", 90)
     with Dojo(entry) as (dojo, s0):
         s1 = dojo.run_cmd(s0, "#eval 1")
         assert isinstance(s1, CommandState) and s1.message == "1"
@@ -17,7 +17,7 @@ def test_example_1(mathlib4_repo: LeanGitRepo) -> None:
         s4 = dojo.run_cmd(s3, "#eval x")
         assert isinstance(s4, CommandState) and s4.message == "1"
 
-        s5 = dojo.run_cmd(s0, "#print smul_sum")
+        s5 = dojo.run_cmd(s0, "#print addMonoidHomLequivNat")
         assert isinstance(s5, CommandState) and s5.message != ""
-        s6 = dojo.run_cmd(s0, "#print linearEquivFunOnFinite")
+        s6 = dojo.run_cmd(s0, "#print addMonoidEndRingEquivInt")
         assert isinstance(s6, LeanError)
