6d0795aa6988c05308495352dcd36e7ff6862ed5
Kaiyu Yang <kaiyuy@meta.com>
2024-09-04 16:58:07 +0000

    bugfix

--- src/lean_dojo/data_extraction/traced_data.py
+++ src/lean_dojo/data_extraction/traced_data.py
@@ -839,7 +839,7 @@ class TracedFile:
     def get_premise_definitions(self) -> List[Dict[str, Any]]:
         """Return all theorems and definitions defined in the current file that
         can be potentially used as premises, including the premises in the theorem
-        statement and premises in the tactics used to prove the theorem. 
+        statement and premises in the tactics used to prove the theorem.
 
         Returns:
             List[Dict[str, Any]]: _description_
